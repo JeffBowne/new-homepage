d019d9d85033bab281df3efcb6b2f75a2099b11a
Jeff Bowne <jeff@jeffbowne.com>
2018-07-29 16:20:04 -0400

    finish animals section

--- animations.js
+++ animations.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
                 var $navLink = $(e.target).closest($('[data-nav-link]'));
             }
 
-            $navLink.addClass('primary-nav__item-link--highlight');
+            $navLink.addClass('link-highlight');
         }, function(e) {
             if ($(e.target) == this) {
                 var $navLink = $(e.target);
@@ -16,7 +16,7 @@ $(document).ready(function() {
                 var $navLink = $(e.target).closest($('[data-nav-link]'));
             }
 
-            $navLink.removeClass('primary-nav__item-link--highlight');
+            $navLink.removeClass('link-highlight');
         }
     );
 
@@ -27,7 +27,7 @@ $(document).ready(function() {
             var $navLink = $(e.target).closest($('[data-nav-link]'));
         }
 
-        $navLink.addClass('primary-nav__item-link--highlight');
+        $navLink.addClass('link-highlight');
     });
 
     $('[data-nav-link]').focusout( function(e) {
@@ -37,7 +37,7 @@ $(document).ready(function() {
             var $navLink = $(e.target).closest($('[data-nav-link]'));
         }
 
-        $navLink.removeClass('primary-nav__item-link--highlight');
+        $navLink.removeClass('link-highlight');
     });
 
     $('[data-nav-link]').on('click', function(e) {
@@ -51,7 +51,6 @@ $(document).ready(function() {
         $('html,body').animate({scrollTop: $(destination).offset().top-36}, 'slow');
     });
 
-
     function positionHighlight(options) {
 
         $(window).scroll(function() { 
@@ -65,39 +64,22 @@ $(document).ready(function() {
         });
     };
 
-    positionHighlight({ section:'#resume-area', element:'.primary-nav__item--resume', specialClass:'primary-nav__item--highlight', aOffset:288, bOffset:168});
-    positionHighlight({ section:'#animals-area', element:'.primary-nav__item--animals', specialClass:'primary-nav__item--highlight', aOffset:288, bOffset:168});
-    positionHighlight({ section:'#pedals-area', element:'.primary-nav__item--pedals', specialClass:'primary-nav__item--highlight', aOffset:288, bOffset:168});
-    positionHighlight({ section:'#design-area', element:'.primary-nav__item--design', specialClass:'primary-nav__item--highlight', aOffset:288, bOffset:168});
-
-
-    function positionHighlight(options) {
-
-        $(window).scroll(function() { 
-            var sPos = $(window).scrollTop();
-
-            if ($(options.section).position().top-options.aOffset <= sPos && $(options.section).position().top + $(options.section).height()+options.bOffset > sPos) {
-                $(options.element).addClass(options.specialClass);
-            } else {
-                $(options.element).removeClass(options.specialClass);
-            };
-        });
-    };
+    positionHighlight({ section:'#resume-area', element:'.primary-nav__item--resume', specialClass:'item-highlight', aOffset:288, bOffset:168});
+    positionHighlight({ section:'#animals-area', element:'.primary-nav__item--animals', specialClass:'item-highlight', aOffset:288, bOffset:168});
+    positionHighlight({ section:'#pedals-area', element:'.primary-nav__item--pedals', specialClass:'item-highlight', aOffset:288, bOffset:168});
+    positionHighlight({ section:'#design-area', element:'.primary-nav__item--design', specialClass:'item-highlight', aOffset:288, bOffset:168});
 
     positionHighlight({ section:'#resume-area', element:'.primary-nav', specialClass:'primary-nav--yellow', aOffset:40, bOffset:168});
     positionHighlight({ section:'#animals-area', element:'.primary-nav', specialClass:'primary-nav--red', aOffset:40, bOffset:168});
     positionHighlight({ section:'#pedals-area', element:'.primary-nav', specialClass:'primary-nav--green', aOffset:40, bOffset:168});
     positionHighlight({ section:'#design-area', element:'.primary-nav', specialClass:'primary-nav--purple', aOffset:40, bOffset:168});
 
-
-
-    $('[data-zoom-frame]').on('click', function(e) {
+    $('[data-gallery-frame]').on('click', function(e) {
         var $frame = $(e.target);
 
-        $frame.clone().addClass('zoom-frame').appendTo('body')
+        $frame.clone().addClass('gallery__frame--zoom').appendTo('body')
         .on('click', function(e) {
             $(e.target).remove()
         });
     });
-
 });
